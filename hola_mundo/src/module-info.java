f4ecdae6c25d762c44e45c5a7f4d66d77ff2aa08
Jaime Serrano Delgado <113890867+jaimeserranodelgado@users.noreply.github.com>
2023-03-10 10:36:09 +0100

    Add files via upload

---
+++ hola_mundo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author inforbach
+ *
+ */
+module hola_mundo {
+}
---
+++ primerosprogramas/src/programas_numeros/SumaNumeros.java
@@ -0,0 +1,10 @@
+package programas_numeros;
+
+public class SumaNumeros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
